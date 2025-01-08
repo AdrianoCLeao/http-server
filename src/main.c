ca65de8ffaa82aeef0d495a0000c94754a310b5e
AdrianoCLeao <adrianocoelho133@gmail.com>
2025-01-07 22:07:44 -0300

    server: add new sketch of an API

--- src/main.c
+++ src/main.c
@@ -1,7 +1,109 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    printf("Hello World!");
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <WS2tcpip.h>
+    #pragma comment(lib, "ws2_32.lib")
+    #define CLOSESOCKET closesocket
+#else
+    #include <unistd.h>
+    #include <arpa/inet.h>
+    #include <sys/socket.h>
+    #define CLOSESOCKET close
+#endif
 
+#define PORT 4000
+#define BUFFER_SIZE 1024
+
+void start_server()
+{
+#ifdef _WIN32
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        fprintf(stderr, "WSAStartup failed: %d\n", WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+#endif
+
+    int server_fd;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    const char *response = "HTTP/1.1 200 OK\r\n"
+                           "Content-Type: text/plain\r\n"
+                           "Content-Length: 13\r\n\r\n"
+                           "Hello, World!";
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0)
+    {
+        perror("Socket creation failed");
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("Bind failed");
+        CLOSESOCKET(server_fd);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("Listen failed");
+        CLOSESOCKET(server_fd);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server running on port %d\n", PORT);
+
+    while (1)
+    {
+        int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if (new_socket < 0)
+        {
+            perror("Accept failed");
+            continue;
+        }
+
+#ifdef _WIN32
+        int bytes_read = recv(new_socket, buffer, BUFFER_SIZE, 0);
+#else
+        int bytes_read = read(new_socket, buffer, BUFFER_SIZE);
+#endif
+
+        if (bytes_read > 0)
+        {
+            printf("Request received:\n%s\n", buffer);
+            send(new_socket, response, strlen(response), 0);
+        }
+        CLOSESOCKET(new_socket);
+    }
+
+    CLOSESOCKET(server_fd);
+#ifdef _WIN32
+    WSACleanup();
+#endif
+}
+
+int main()
+{
+    start_server();
     return 0;
 }
